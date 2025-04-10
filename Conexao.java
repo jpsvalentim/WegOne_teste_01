492611454bc5db82758ff787868944faf4836b9f
João Pedro Silva Valentim <j.pedro__@hotmail.com>
2025-04-10 02:25:27 -0300

    s
    s

--- Conexao.java
+++ Conexao.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class Conexao {
     private static final String URL = "jdbc:mysql://localhost:3306/wegone_db";
     private static final String USUARIO = "root";
-    private static final String SENHA = "sua_senha_aqui";
+    private static final String SENHA = "root";
 
     public static Connection getConexao() {
         try {
