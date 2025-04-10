a28e6a0e17084b572987f4d1b15d2950fc0fb528
João Pedro Silva Valentim <j.pedro__@hotmail.com>
2025-04-10 10:08:52 -0300

    aa
    a

--- TesteConexao.java
+++ TesteConexao.java
@@ -3,6 +3,13 @@ import java.sql.Connection;
 public class TesteConexao {
     public static void main(String[] args) {
         Connection conn = Conexao.getConexao();
-        System.out.println("Conexão realizada com sucesso!"+conn);
+
+        if (conn != null) {
+            System.out.println("Conexão estabelecida com sucesso!");
+        } else {
+            System.out.println("Falha na conexão.");
+        }
+        
     }
 }
+
